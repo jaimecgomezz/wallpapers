5b41523d9b6ce99d3011bd219235e9e3ba8b6d09
thbrd <jaimecgomezwork@gmail.com>
2022-10-30 20:17:20 -0600

    style: Format

--- readmify.sh
+++ readmify.sh
@@ -9,11 +9,11 @@
 ##############################
 
 for theme in */; do
-  echo -e "# Preview" >"${theme}/README.md"
-  for filename in "${theme}"*; do
-    file="$( echo "${filename}" | sed "s_${theme}__")"
-    name="$( echo "$file" | sed 's/\..*//')"
-    echo -e "![${name}](${file} '${name}')" >>"${theme}/README.md"
-  done
-  sed -i '/README.md/d' "${theme}/README.md"
+	echo -e "# Preview" >"${theme}/README.md"
+	for filename in "${theme}"*; do
+		file="$(echo "${filename}" | sed "s_${theme}__")"
+		name="$(echo "$file" | sed 's/\..*//')"
+		echo -e "![${name}](${file} '${name}')" >>"${theme}/README.md"
+	done
+	sed -i '/README.md/d' "${theme}/README.md"
 done
